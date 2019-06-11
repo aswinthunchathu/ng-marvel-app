7b9e2bb855fdf44a519154cb7f8d3a91ee87e9ba
Aswin Varghese <aswin.thunchathu@gmail.com>
2019-06-11 14:47:10 +0200

    added card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { CardComponent } from './shared/card/card.component'
+import { CardComponent } from './shared/card/card.component';
+import { CharactersComponent } from './characters/characters.component'
 
 @NgModule({
-    declarations: [AppComponent, CardComponent],
+    declarations: [AppComponent, CardComponent, CharactersComponent],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
