8b2ab2d00e7638873a01ed75467444535fdf848d
Aswin Varghese <aswin.thunchathu@gmail.com>
2019-06-14 10:38:54 +0200

    added comic details store

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -2,16 +2,19 @@ import { ActionReducerMap } from '@ngrx/store'
 
 import * as fromCharactersReducer from '../characters/store/characters.reducer'
 import * as fromComicsReducer from '../comics/store/comics.reducer'
+import * as fromComicReducer from '../comics/comic-details/store/comics.reducer'
 import * as fromCharacterReducer from '../characters/character-details/store/character.reducer'
 
 export interface AppState {
     characters: fromCharactersReducer.State
     comics: fromComicsReducer.State
     character: fromCharacterReducer.State
+    comic: fromComicReducer.State
 }
 
 export const appReducer: ActionReducerMap<AppState> = {
     characters: fromCharactersReducer.charactersReducer,
     comics: fromComicsReducer.comicsReducer,
     character: fromCharacterReducer.characterReducer,
+    comic: fromComicReducer.comicReducer,
 }
