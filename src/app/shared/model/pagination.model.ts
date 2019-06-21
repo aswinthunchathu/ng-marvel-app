64cd4e101322a5d2360e3b0b653ad206c9d27a0a
Aswin Varghese <aswin.thunchathu@gmail.com>
2019-06-21 12:41:34 +0200

    temprory bug fix

--- src/app/shared/model/pagination.model.ts
+++ src/app/shared/model/pagination.model.ts
@@ -2,7 +2,7 @@ export class Pagination {
     constructor(public offset: number, public limit: number, public total: number, public count: number) {}
 
     get hasMore() {
-        return this.offset > -1 ? this.total > this.limit + this.offset : true
+        return this.offset > -1 ? this.total > this.limit + this.offset : false
     }
 
     get nextPage() {
